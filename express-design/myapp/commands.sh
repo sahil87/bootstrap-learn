91b1b2251e5db775f4c0a753f60d045e6b9a1b52
Sahil Ahuja <sahilahuja@gmail.com>
2015-07-10 12:24:04 +0530

    Arranging

---
+++ express-design/myapp/commands.sh
@@ -0,0 +1,12 @@
+#sudo npm install -g bower
+#npm install --save-dev gulp gulp-nodemon browser-sync
+#npm install --save express
+#npm install express-generator
+#express -c sass --hbs myapp #Sass and handlebar templates support
+
+#For every fresh install:
+#bower install
+#npm install
+
+#DEBUG=myapp:* npm start
+gulp
