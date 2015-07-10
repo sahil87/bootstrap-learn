217eb16ceb29afa3f271199ed2129a9a222cced8
Sahil Ahuja <sahilahuja@gmail.com>
2015-07-10 12:22:44 +0530

    Arranging

--- browsersync-design/commands.sh
+++ browsersync-design/commands.sh
@@ -1,13 +1,8 @@
 #sudo npm install -g bower
-#bower install
+#npm install --save-dev browser-sync
 
-#ONE TIME:
-#npm install --save-dev gulp gulp-nodemon browser-sync
-#npm install express --save
-#npm install express-generator
-#express -c sass --hbs myapp #Sass and handlebar templates support
-
+#For every fresh install:
 #bower install
-#DEBUG=myapp:* npm start
+#npm install
 
 browser-sync start --config browser-sync-config.js
