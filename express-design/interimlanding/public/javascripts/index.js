2bad529e0cec1bfd93b2a092f6473b50b357104e
Sahil Ahuja <sahilahuja@gmail.com>
2015-07-27 11:42:06 +0000

    changed page scrolling frameworking to pagepiling.js

--- express-design/interimlanding/public/javascripts/index.js
+++ express-design/interimlanding/public/javascripts/index.js
@@ -1,68 +1,95 @@
 var SC = {};
 
 $(document).ready(function() {
-  $('#fullpage').fullpage({
+  $('#pagepiling').pagepiling({
     anchors: ['sportscafe', 'allsports', 'youridols', 'whyus', 'aboutus'],
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['SportCafe', 'All Sports', 'Your Idols', 'Why us', 'About US'],
+    //animateAnchor: true,
+    menu: "#menuanchor",
+    navigation: {
+      'textColor': '#000',
+      'bulletsColor': '#000',
+      'position': 'right',
+      'tooltips': ['SportCafe', 'All Sports', 'Your Idols', 'Why Us', 'About']
+    },
     //afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
     afterLoad: function(anchorLink, index) {
       var loadedSlide = $(this);
-      switch(anchorLink) {
+      switch (anchorLink) {
 
-        case "sportscafe": {
-          SC.page1Animate();
-          break;
-        }
-        case "allsports": {
+        case "sportscafe":
+          {
+            SC.page1Animate();
+            break;
+          }
+        case "allsports":
+          {
             SC.page2Animate();
             break;
-        }
-        case "youridols": {
+          }
+        case "youridols":
+          {
             SC.page3Animate();
             break;
-        }
+          }
 
         default:
           break;
       }
-      $("#navbar").find(".active").removeClass("active");
-      $("#navbar").find("a:hover").removeClass("a:hover");
-      $("#navbar").find("a:focus").removeClass("a:focus");
-      $('#navbar a[href="#' + anchorLink + '"]').parent().addClass("active");
+
+      // $("#navbar").find("a:hover").removeClass("a:hover");
+      // $("#navbar").find("a:focus").removeClass("a:focus");
+      // $('#navbar a[href="#' + anchorLink + '"]').parent().addClass("active");
     },
     onLeave: function(index, nextIndex, direction) {
       var leavingSection = $(this);
       if (index == 1) {
-        SC.testAnim('#page1-text-container',"hinge");
-        setTimeout(function(){
+        SC.testAnim('#page1-text-container', "hinge");
+        setTimeout(function() {
           $("#page1-text-container").children().addClass("page1-text");
         }, 500);
       }
+    },
+    afterRender: function() {
+      SC.page1Animate();
     }
   });
 });
 
 SC.testAnim = function(element, x) {
-  $(element).removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+  $(element).removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
     $(this).removeClass();
   });
 };
 
 SC.page1Animate = function() {
-  setTimeout(function(){SC.testAnim('#page1-logo', "fadeIn");}, 3000);
-  setTimeout(function(){SC.testAnim('#page1-text1', "fadeInLeft");}, 2000);
-  setTimeout(function(){SC.testAnim('#page1-divider1', "fadeIn");}, 2800);
-  setTimeout(function(){SC.testAnim('#page1-text2', "fadeInUp");}, 2200);
-  setTimeout(function(){SC.testAnim('#page1-divider2', "fadeIn");}, 2800);
-  setTimeout(function(){SC.testAnim('#page1-text3', "fadeInRight");}, 2200);
+  setTimeout(function() {
+    SC.testAnim('#page1-logo', "fadeIn");
+  }, 3000);
+  setTimeout(function() {
+    SC.testAnim('#page1-text1', "fadeInLeft");
+  }, 2000);
+  setTimeout(function() {
+    SC.testAnim('#page1-divider1', "fadeIn");
+  }, 2800);
+  setTimeout(function() {
+    SC.testAnim('#page1-text2', "fadeInUp");
+  }, 2200);
+  setTimeout(function() {
+    SC.testAnim('#page1-divider2', "fadeIn");
+  }, 2800);
+  setTimeout(function() {
+    SC.testAnim('#page1-text3', "fadeInRight");
+  }, 2200);
 };
 
 SC.page2Animate = function() {
-  setTimeout(function(){SC.testAnim('#page2-text', "zoomIn");}, 500);
+  setTimeout(function() {
+    SC.testAnim('#page2-text', "zoomIn");
+  }, 500);
 };
 
 SC.page3Animate = function() {
-  setTimeout(function(){SC.testAnim('#page3-text', "bounceInLeft");}, 0);
+  setTimeout(function() {
+    SC.testAnim('#page3-text', "bounceInLeft");
+  }, 0);
 };
