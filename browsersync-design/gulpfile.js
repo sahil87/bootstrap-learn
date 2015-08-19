c20fb29decdabaa84eb2f66c22874655f7433a89
Sahil Ahuja <sahilahuja@gmail.com>
2015-08-19 08:01:55 +0000

    Making project ready for Rana

--- browsersync-design/gulpfile.js
+++ browsersync-design/gulpfile.js
@@ -3,22 +3,22 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 //var nodemon = require('gulp-nodemon');
-var sass = require('gulp-sass');
+//var sass = require('gulp-sass');
 
 //gulp.task('default', ['browser-sync'], function() {});
 
-gulp.task('default', ['sass', 'sass:watch', 'browser-sync']);
+gulp.task('default', ['browser-sync']);
 
-gulp.task('sass:watch', function () {
-  console.log("Starting sass-watch.");
-  gulp.watch('./design/scss/**/*.scss', ['sass']);
-});
+//gulp.task('sass:watch', function () {
+//  console.log("Starting sass-watch.");
+//  gulp.watch('./design/scss/**/*.scss', ['sass']);
+//});
 
-gulp.task('sass', function () {
-  gulp.src(['./design/scss/**/*.scss'])
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./design/css/'));
-});
+//gulp.task('sass', function () {
+// gulp.src(['./design/scss/**/*.scss'])
+//  .pipe(sass.sync().on('error', sass.logError))
+//   .pipe(gulp.dest('./design/css/'));
+//});
 
 gulp.task('browser-sync', function() {
   console.log("Starting browser-sync.");
